058c1a10ab83273ecbfdebc98a25cdc0ee648e6d
lonce wyse <lonce.acad@zwhome.org>
2023-02-08 11:55:52 +0000

    added --nocerts flag in case you want to run on a local machine without certificates for https

--- multiServerExpress.js
+++ multiServerExpress.js
@@ -3,32 +3,42 @@ const fs = require('fs');
 const app = express();
 const http = require('http');
 
-const https = require('https'); //*
+const https = require('https');
+
+const { program } = require('commander');
+program
+    .option('--nocerts')
+program.parse();
+
 
 //const { Server } = require('socket.io');
 //const socketio = new Server(server); 
 
 var portnum = process.argv[2] || 3000; // Get portnum from the command line if it is there, otherwise use 3000 as default
 
+var theServer;
 //Certificate for Domain 1
-const privateKey1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/privkey.pem', 'utf8');
-const certificate1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/cert.pem', 'utf8');
-const ca1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/chain.pem', 'utf8');
-const credentials1 = {
-    key: privateKey1,
-    cert: certificate1,
-    ca: ca1
-};
+if (! program.opts().nocerts) {  // don't check for certs on your local machine
+    const privateKey1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/privkey.pem', 'utf8');
+    const certificate1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/cert.pem', 'utf8');
+    const ca1 = fs.readFileSync('/etc/letsencrypt/live/appskynote.com/chain.pem', 'utf8');
+    const credentials1 = {
+	key: privateKey1,
+	cert: certificate1,
+	ca: ca1
+    };
+    theServer = https.createServer(credentials1,app);
+} else {
+    theServer = http.createServer(app);
+}
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials1,app);
 
 // Use www as the "root" directory for all requests.
 // if no path is given, it will look for index.html in that directoy.
 app.use(express.static('www'));
 
 // Start the server listening on a port 
-httpsServer.listen(portnum, function(){
+theServer.listen(portnum, function(){
 	console.log ("server listening on port " + portnum);
 });
 
