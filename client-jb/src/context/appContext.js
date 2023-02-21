0cb99276ad3d153c5cad5195befe88417b7baf37
Dilip Harish <dilip.harish01@estudiant.upf.edu>
2023-02-21 12:47:52 +0100

    made changes to include createProxyMiddleWare.js to reverse proxy using express from frontend react to backend node

--- client-jb/src/context/appContext.js
+++ client-jb/src/context/appContext.js
@@ -8,6 +8,9 @@ import {
   LOGIN_USER_BEGIN,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
 } from "./actions";
 
 import reducer from "./reducer";
@@ -109,6 +112,34 @@ const AppProvider = ({ children }) => {
     }
     clearAlert();
   };
+
+  const setupUser = async (currentUser, endPoint, alertText) => {
+    dispatch({ type: SETUP_USER_BEGIN });
+    try {
+      const { data } = await axios.post(
+        `/api/v1/auth/${endPoint}`,
+        currentUser
+      );
+
+      const { user, token, location } = data;
+      dispatch({
+        type: SETUP_USER_SUCCESS,
+        payload: {
+          user,
+          token,
+          location,
+        },
+      });
+      addUserToLocalStorage({ user, token, location });
+    } catch (error) {
+      dispatch({
+        type: SETUP_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
