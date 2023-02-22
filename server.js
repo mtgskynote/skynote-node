2168ec44e62897473023b0f1e9acb8e8b371a770
Dilip Harish <dilip.harish01@estudiant.upf.edu>
2023-02-22 10:48:21 +0100

    changed default port to 5000 if PORT_NUMBER is not mentioned in .env file

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/api/v1/jobs", jobsRoutes);
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleWare);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 //const port = process.env.PORT;
 
 const start = async () => {
