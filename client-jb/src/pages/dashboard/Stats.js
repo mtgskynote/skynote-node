4c093de00262eb019dc53d779d0585b282c71bfd
Dilip Harish <dilip.harish01@estudiant.upf.edu>
2023-04-11 10:53:17 +0200

    added chartjs and doughnut chart files

--- client-jb/src/pages/dashboard/Stats.js
+++ client-jb/src/pages/dashboard/Stats.js
@@ -1,3 +1,8 @@
+import "./Stats.css";
+import { GiProgression, GiProfit } from "react-icons/gi";
+import { HiWorms } from "react-icons/hi";
+import { CircularProgressBar } from "react-circular-progressbar";
+
 const Stats = () => {
   return <h1>Stats</h1>;
 };
