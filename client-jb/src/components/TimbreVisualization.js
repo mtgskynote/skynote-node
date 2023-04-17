05b0fc574e924d95c62e8ab08abcade6d5f65799
Abhishek Choubey <abhishek.choubey01@estudiant.upf.edu>
2023-04-17 11:13:14 +0200

    added pitch detection

--- client-jb/src/components/TimbreVisualization.js
+++ client-jb/src/components/TimbreVisualization.js
@@ -2,60 +2,93 @@ import React, { useRef, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PieChart from "../purecomponents/pieChart";
 import { useControlBar } from "../purecomponents/controlbar";
-import {makeAudioStreamer} from '../purecomponents/audioStreamer.js';
+import {
+  makeAudioStreamer,
+  getPitch,
+} from "../purecomponents/audioStreamer.js";
 
 const randInt = function (min, max) {
   return Math.floor(min + (max + 1 - min) * Math.random());
 };
-
 
 const TimbreVisualization = () => {
   //let ref = useRef(0);
   //const params = useParams();
   //console.log(`${folderBasePath}/${params.file}`);
 
-  const [segments, setSegments] = useState([Math.random(), Math.random(), Math.random(), Math.random(), Math.random()]);
+  const [segments, setSegments] = useState([
+    Math.random(),
+    Math.random(),
+    Math.random(),
+    Math.random(),
+    Math.random(),
+  ]);
 
-  console.log(`initializing a new timbre visualization!!!!!!!!!`)
-  const audioStreamer=makeAudioStreamer();
-  const labels=["sweetness", "warmth", "vibrato", "tremelo", "stringency", "loudness", "roughness"]
-
+  //console.log(`initializing a new timbre visualization!!!!!!!!!`);
+  const audioStreamer = makeAudioStreamer();
+  const labels = [
+    "sweetness",
+    "warmth",
+    "vibrato",
+    "tremelo",
+    "stringency",
+    "loudness",
+    "roughness",
+  ];
 
   const controlbar = useControlBar();
 
-  useEffect(() => {
+  function normalizeFrequency(frequency) {
+    const minFrequency = 20;
+    const maxFrequency = 20000;
+    return (frequency - minFrequency) / (maxFrequency - minFrequency);
+  }
 
-    let playbutton=document.getElementById("play")
-    console.log(`playbutton is ${playbutton}`)
-     
-    playbutton.addEventListener('click', function(){
-       audioStreamer.init();
+  useEffect(() => {
+    let playbutton = document.getElementById("play");
+    console.log(`playbutton is ${playbutton}`);
+
+    playbutton.addEventListener("click", function () {
+      audioStreamer.init();
     });
-  
-    const intervalId1 = setInterval(() => {
+
+    const intervalId1 = setInterval(async () => {
       let a = audioStreamer.getAmplitude();
-      console.log(`a=${a}`)
-      setSegments([.5, .5, .5, .5, .5, a])
-      //setSegments([Math.random(), Math.random(), Math.random(), Math.random(), Math.random()])
-    },100 );
-    
+      try {
+        let pitch = await audioStreamer.getPitch();
+        let normalizedPitch = normalizeFrequency(pitch) * 100;
+        //console.log(`pitch=${pitch}, normalizedPitch=${normalizedPitch}`);
+        setSegments([0.5, 0.5, 0.5, normalizedPitch, 0.5, a]);
+      } catch (error) {
+        //console.log("Error getting pitch:", error);
+      }
+    }, 100);
+
     // Clean up interval on component unmount
     return () => {
       clearInterval(intervalId1);
     };
-
   }, []); // Empty dependency array to run effect only once on component mount
-
-
 
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <PieChart m_width={440} m_height={440} radius={180} segments={segments} labels={labels} />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <PieChart
+          m_width={440}
+          m_height={440}
+          radius={180}
+          segments={segments}
+          labels={labels}
+        />
       </div>
       <div>{controlbar}</div>
     </div>
   );
-
 };
 export default TimbreVisualization;
