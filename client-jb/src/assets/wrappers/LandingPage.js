dd45ac5077ee4affbf3c666525965b4ce188c6a0
Dilip Harish <dilip.harish01@estudiant.upf.edu>
2023-05-17 13:11:05 +0200

    made changes to landing page to mimic the static page

--- client-jb/src/assets/wrappers/LandingPage.js
+++ client-jb/src/assets/wrappers/LandingPage.js
@@ -27,13 +27,44 @@ const Wrapper = styled.main`
   .main-img {
     display: none;
   }
-  @media (min-width: 992px) {
+  /* @media (min-width: 992px) {
     .page {
       grid-template-columns: 1fr 1fr;
       column-gap: 3rem;
     }
     .main-img {
       display: block;
+    }
+    
+  } */
+
+  @media screen and (min-width: 768px){
+    section: {
+      padding: 1em 7em;
+    }
+  }
+
+  @media screen  and (min-width: 992px){
+
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+    
+    section: {
+      padding: 3em;
+    }
+
+    .card {
+      padding: 10em 1em;
+    }
+    .column{
+      flex: 0 0 33.33%;
+      max-width: 33.33%;
+      padding: 0 1em;
     }
   }
   .primary-text {
@@ -42,7 +73,7 @@ const Wrapper = styled.main`
   }
   .primary-subheading {
   font-weight: 700;
-  color: #fe9e0d;
+  color: #505050;
   font-size: 1.15rem;
   }
 
@@ -95,6 +126,69 @@ const Wrapper = styled.main`
 .work-section-info svg {
   width: 2em;
   height: 2em;
+}
+.row{
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2em 1em;
+  text-align: center;
+}
+.column{
+  width: 50%;
+  padding: 3em; 
+  }
+.card{
+  box-shadow: 0 0 2.5em rgba(25, 0, 58, 0.15);
+  padding: 2em;
+  border-radius: 1em;
+  color: #1f003b;
+  transition: all 0.3s ease-in-out;
+  background-color: #ffffff;
+}
+.card .img-container{
+  width: 8em;
+  height: 8em;
+  background-color: #1f003b;
+  padding: 0.5em;
+  border-radius: 60%;
+  margin: 0 auto;
+}
+.card img{
+  width: 90%;
+  border-radius: 50%;
+}
+.card h3{
+  padding: 1em 0 0.5em 0;
+  font-weight: 700;
+  text-align: center;
+}
+.card p{
+  font-weight: 300; 
+  margin: 2em 0 2em 0;
+  text-transform: capitalize;
+  letter-spacing: 1px;
+}
+.card a{
+  text-decoration: none;
+  color: inherit;
+  font-size: 1.4em;
+  text-align: center;
+}
+.card:hover{
+  background: linear-gradient(
+    #6045ea, #8567f7
+  );
+  color: #ffffff;
+}
+
+.card:hover .img-container{ 
+  transform: scale(1.15)
+}
+.icons{
+  margin: auto;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 }
 `
 export default Wrapper
--- client-jb/src/pages/Landing.js
+++ client-jb/src/pages/Landing.js
@@ -1,11 +1,32 @@
 import { Logo } from "../components"; // Named export
-import { FaQuestionCircle, FaPeopleArrows, FaMusic } from 'react-icons/fa';
+import { FaQuestionCircle, FaPeopleArrows, FaMusic , FaTwitter, FaLinkedinIn, FaGithub} from 'react-icons/fa';
 import main from "../assets/images/main.svg";
+import waddellimg from "../assets/images/team/GeorgeWaddell.jpeg";
+import lonceimg from "../assets/images/team/lonce_cnm_bust_sm2.jpg";
+import rafaelimg from "../assets/images/team/RafaelRamirez.png";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const workInfoData = [
+  {
+    image: <FaQuestionCircle/>,
+    title: "Why use it?",
+    text: "It empowers students with tools to detect and correct errors, build awareness of their technique, and allow them to practise more efficiently alone or in a class setting.",
+  },
+  {
+    image: <FaPeopleArrows/>,
+    title: "Who is it for?",
+    text: "It is designed with a wide range of student levels in mind, from beginners to professional players.",
+  },
+  {
+    image: <FaMusic/>,
+    title: "Personalized Learning",
+    text: "Personalized practice based on individual goals and achievements. A dynamic pedagogical system with multiple possible paths to enhance your music level and tailor your skills development.",
+  },
+];
+
+const videoInfoData = [
   {
     image: <FaQuestionCircle/>,
     title: "Why use it?",
@@ -47,26 +68,98 @@ const Landing = () => {
       </div>
 
       <div className="work-section-wrapper">
-      <div className="work-section-top">
-        <p className="primary-subheading">SkyNote</p>
-        <h1 className="primary-heading">SkyNote</h1>
-        {/* <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
-        </p> */}
-      </div>
-      <div className="work-section-bottom">
-        {workInfoData.map((data) => (
-          <div className="work-section-info" key={data.title}>
-            {data.image}
-            <div className="info-boxes-img-container">
+        <div className="work-section-top">
+          <h1 className="primary-heading">SkyNote</h1>
+        </div>
+        <div className="work-section-bottom">
+          {workInfoData.map((data) => (
+            <div className="work-section-info" key={data.title}>
+              {data.image}
+              <div className="info-boxes-img-container">
+              </div>
+              <h2>{data.title}</h2>
+              <p>{data.text}</p>
             </div>
-            <h2>{data.title}</h2>
-            <p>{data.text}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="work-section-wrapper">
+        <div className="work-section-top">
+          <h1 className="primary-heading">Artificial Intelligence</h1>
+          <div><br></br></div>
+          <h2 className="primary-subheading">Sound quality, posture, intonation, pitch, rhythm and expression in music performance</h2>
+        </div>
+        <div className="work-section-bottom">
+          {videoInfoData.map((data) => (
+            <div className="work-section-info" key={data.title}>
+              
+              <div className="info-boxes-img-container">
+              </div>
+              <div class="ratio ratio-1x1">
+                <iframe src="https://appskynote.com/skynote-media/videos/Skynote1-SoundQuality.mp4#t=15" title="YouTube video" allowfullscreen></iframe>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* meet the team with three tiles */}
+    
+      {/* <div className="work-section-top">
+        <div className="primary-heading">
+            <h1 class="heading">
+              Meet the <span> Team </span>
+            </h1>
+            <div className="profile"></div>
+        </div>
+      </div> */}
+
+      <div className="work-section-top">
+        <h1> Our Team</h1>
+        <div><br></br></div>
+        <h2 className="primary-subheading">We are a team of researchers and developers experts in music and machine learning</h2>
+      </div>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+              <div className="img-container">
+                <img src={lonceimg}/>
+              </div>
+              <h3>Dr. Lonce Wyse</h3>
+              <p>PhD Cognitive & Neural Systems Deep Learning and Audio, Music Technology Group(UPF)</p>
+              <div class="icons">
+                <a href="https://www.linkedin.com/in/lonce/" target="_blank"><FaLinkedinIn/></a>
+              </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+              <div className="img-container">
+                <img src={rafaelimg}/>
+              </div>
+              <h3>Dr. Rafael Ramirez</h3>
+              <p>PhD in Computer Science, Head of the Music and Machine Learning Lab (UPF)</p>
+              <div class="icons">
+                <a href="https://www.linkedin.com/in/rafaelr2/" target="_blank"><FaLinkedinIn/></a>
+              </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+              <div className="img-container">
+                <img src={waddellimg}/>
+              </div>
+              <h3>Dr.George Waddell</h3>
+              <p>PhD Music Performance Research Associate, Royal College of Music</p>
+              <div class="icons">
+                <a href="https://www.linkedin.com/in/drgeorgewaddell/" target="_blank"><FaLinkedinIn/></a>
+              </div>
+          </div>
+        </div>
+      </div>  
+
     </Wrapper>
   );
 };
