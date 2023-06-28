45758b75cd7102fd35b784b4ab3a4dc21a3ff9f6
Dilip Harish <dilip.harish01@estudiant.upf.edu>
2023-06-28 13:13:19 +0200

    removed Navbar

--- client-jb/src/components/AllLessons.js
+++ client-jb/src/components/AllLessons.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Col, Row, Card, Button } from "react-bootstrap";
 
-// import ViolinCard from "./violinImageCard";
+import ViolinCard from "./violinImageCard";
 
-// import violinImg from "../assets/images/violin/violinDisplay.jpg";
-// import violinImg2 from "../assets/images/violin/violin1.jpeg";
+import violinImg from "../assets/images/violin/violinDisplay.jpg";
+import violinImg2 from "../assets/images/violin/violin1.jpeg";
 
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -104,7 +104,24 @@ const AllLessons = () => {
     <TreeItem
       key={nodes.id}
       nodeId={nodes.id}
-      label={nodes.name}
+      label={
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ marginRight: "10px", fontSize: 18 }}>{nodes.name}</div>
+          <div
+            style={{
+              width: "50px",
+              height: "50px",
+              background: "transparent",
+              color: "white",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {nodes.name}
+          </div>
+        </div>
+      }
       sx={{
         "& .MuiTreeItem-iconContainer": {
           position: "absolute",
@@ -174,6 +191,93 @@ const AllLessons = () => {
       >
         {renderTree(levelThreeData)}
       </TreeView>
+
+      <div className="center" style={{ marginTop: "2rem" }}>
+        <h2>Level One</h2>
+      </div>
+      <div className="center">
+        <Row xs={1} md={3} className="g-4" style={{ marginTop: "2rem" }}>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelone"
+              ButtonText="Basic Bowing"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/leveltwo"
+              ButtonText="Level Two"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelthree"
+              ButtonText="Level Three"
+            />
+          </Col>
+        </Row>
+      </div>
+
+      <div className="center" style={{ marginTop: "2rem" }}>
+        <h2>Level Two</h2>
+      </div>
+      <div className="center">
+        <Row xs={1} md={3} className="g-4" style={{ marginTop: "2rem" }}>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelone"
+              ButtonText="Advanced Bowing"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/leveltwo"
+              ButtonText="Level Two"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelthree"
+              ButtonText="Level Three"
+            />
+          </Col>
+        </Row>
+      </div>
+
+      <div className="center" style={{ marginTop: "2rem" }}>
+        <h2>Level Three</h2>
+      </div>
+      <div className="center">
+        <Row xs={1} md={3} className="g-4" style={{ marginTop: "2rem" }}>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelone"
+              ButtonText="Basic Bowing"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/leveltwo"
+              ButtonText="Level Two"
+            />
+          </Col>
+          <Col>
+            <ViolinCard
+              violinImg={violinImg2}
+              subfolder="/levels/levelthree"
+              ButtonText="Level Three"
+            />
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
--- client-jb/src/components/index.js
+++ client-jb/src/components/index.js
@@ -1,19 +1,9 @@
 import Logo from "./Logo";
 import FormRow from "./FormRow";
 import Alert from "./Alert";
-import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
 import Header from "./Header";
 import LineChart from "./LineChart";
 
-export {
-  Logo,
-  FormRow,
-  Alert,
-  Navbar,
-  BigSidebar,
-  SmallSidebar,
-  Header,
-  LineChart,
-};
+export { Logo, FormRow, Alert, BigSidebar, SmallSidebar, Header, LineChart };
