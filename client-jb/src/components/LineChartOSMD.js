acb25c5437a70c314b0d1cac58cd1cff557cb461
Amaia Sagasti <amaiasagastim@gmail.com>
2023-09-20 10:40:00 +0200

    scaled pitch data tracking

--- client-jb/src/components/LineChartOSMD.js
+++ client-jb/src/components/LineChartOSMD.js
@@ -1,25 +1,72 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
+//Convert frequency Hertz to MIDI function
+const freq2midipitch = (freq) => {
+  return(12 * (Math.log2(freq / 440)) + 69)
+}
+let staffBox;
+let container;
+
+//Pitch track line component
 const LineChart = ({ pitchData }) => {
-  if (!pitchData || pitchData.length === 0) return null;
+  //if (!pitchData || pitchData.length === 0) return null;
 
-  // Normalize pitch data between 0 and 1
-  const normalizedData = pitchData.map(
-    (value) =>
-      (value - Math.min(...pitchData)) /
-      (Math.max(...pitchData) - Math.min(...pitchData))
-  );
+  const [previousPitchData, setPreviousPitchData] = useState([]);
+  const [polylinePoints, setPolylinePoints] = useState('');
+  
+  // Normalize pitch data between 0 (A3) and 1 (C6)
+  let minimumMIDI=57 //A3
+  let maximumMIDI=84 //C6
 
   const spacing = 10; // Spacing between points
   const svgWidth = pitchData.length * spacing;
 
-  // Calculate the polyline points
-  const polylinePoints = normalizedData
-    .map((value, index) => `${index * spacing},${100 - value * 100}`)
-    .join(" ");
+  useEffect(()=> {
+    // New values added to pitchdata
+    const newValues = pitchData.filter((value) => !previousPitchData.includes(value));
+    const newNormalizedData = newValues.map(
+      (value) =>
+        (freq2midipitch(value) - minimumMIDI) /
+        (maximumMIDI - minimumMIDI)
+    )
+    // Actualizar el estado anterior con el nuevo estado
+    setPreviousPitchData(pitchData);
+    
+  
+    // Combinar los nuevos datos procesados con los datos anteriores
+    setPolylinePoints((prevPolylinePoints) => {
+      // Mapear y agregar las coordenadas de los nuevos valores al estado anterior
+      const newPolylinePoints = newNormalizedData.map((value, index) => {
+        const x = (prevPolylinePoints.split(' ').length + index) * spacing;
+        const y = 100 - value * 100;
+        return `${x},${y}`;
+        });
+      // Combinar las coordenadas anteriores con las nuevas
+      return prevPolylinePoints + ' ' + newPolylinePoints.join(' ');
+    });
+  
+  }, [pitchData, previousPitchData]);
+
+  // Style
+  /*container = document.getElementById('osmdSvgPage1');
+  staffBox = container.querySelector('staffline'); 
+  let topValue=0
+  let leftValue=0
+  if (staffBox){
+    const coord = staffBox.getBoundingClientRect();
+    topValue=coord.top
+    leftValue=coord.left
+  }*/
+
+  const style = {
+    overflowX: "auto",
+    /*position: "absolute",
+    top: topValue, // Ajusta la posición vertical según tus necesidades
+    left: leftValue, // Ajusta la posición horizontal según tus necesidades*/
+  };
 
   return (
-    <div style={{ overflowX: "auto" }}>
+    <div style={style}>
       <svg>
         <polyline
           points={polylinePoints}
