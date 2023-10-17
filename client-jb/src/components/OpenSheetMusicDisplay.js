85f7d00ea5051b96ae525af479d52b42acb4d9f0
Amaia Sagasti <amaiasagastim@gmail.com>
2023-10-17 12:53:19 +0200

    more midi to staff mapping values

--- client-jb/src/components/OpenSheetMusicDisplay.js
+++ client-jb/src/components/OpenSheetMusicDisplay.js
@@ -31,6 +31,18 @@ const midi2StaffGaps=(playedNoteMidi)=>{
   // Create a mapping of MIDI note values to staff offsets
   const midiToStaffMapping = {
     // Define mappings for specific MIDI notes
+    48:13,     // C3
+    49:12.5,   // C#3
+    50:12,     // D3
+    51:11.5,   // D#3
+    52:11,     // E3
+    53:10,     // F3
+    54:9.5,    // F#3
+    55:9,      // G3
+    56:8.5,    // G#3
+    57:8,      // A3
+    58:7.5,    // A#3
+    59:7,      // B3
     60: 6,     // C4
     61: 5.5,   // C#4
     62: 5,     // D4
@@ -41,19 +53,19 @@ const midi2StaffGaps=(playedNoteMidi)=>{
     67:2,      // G4
     68:1.5,    // G#4
     69:1,      // A4
-    70:0.5,   // A#4
-    71:0,     // B4 --> middle line of the staff
+    70:0.5,    // A#4
+    71:0,      // B4 --> middle line of the staff
     72:-1,     //C5
-    73:-1.5,     //C#5
+    73:-1.5,   //C#5
     74:-2,     //D5
-    75:-2.5,     //D#5
+    75:-2.5,   //D#5
     76:-3,     //E5
     77:-4,     //F5
-    78:-4.5,     //F#5
+    78:-4.5,   //F#5
     79:-5,     //G5
-    80:-5.5,     //G#5
+    80:-5.5,   //G#5
     81:-6,     //A5
-    82:-6.5,     //A#5
+    82:-6.5,   //A#5
     83:-7,     //B5
     84:-8,     //C6
   };
