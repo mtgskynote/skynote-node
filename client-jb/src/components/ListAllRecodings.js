ab38e650b3cfad6ef77a3ba36b1a7c3d276b86cc
Amaia <amaiasagastim@gmail.com>
2024-01-18 10:51:10 +0100

    new function call to DB getALLrecData

---
+++ client-jb/src/components/ListAllRecodings.js
@@ -0,0 +1,154 @@
+// ListAllRecordings.js
+import React, { useState, useEffect } from 'react';
+import { json, useLocation, useNavigate } from 'react-router-dom';
+import ListAllRecordingsCSS from './ListRecordings.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getRecData, getAllRecData, deleteRecording } from "../utils/studentRecordingMethods.js";
+import { useAppContext } from "../context/appContext";
+
+import {
+  faTrash,
+  faPlay,
+  faEye,
+  faStar
+} from "@fortawesome/free-solid-svg-icons";
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////IMPORTANT READ ME!!//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  THIS PART IS FOR DATABASE PETITIONS. ATM YOU CAN READ AND DELETE ENTRIES 
+//  ATM IT GETS THE USER ID FROM getCurrentUser
+//  AND THEN IT REQUESTS THE RECORDINGS FROM THAT USER AND THAT SPECIFIC SONG. WE DON'T HAVE A WAY
+//  TO GET THE SONG ID RIGHT NOW, SO WE HAVE A PLACEHOLDER ONE
+//  THE DELETE OPTION IS WORKING ON THE DATABASE SIDE, BUT DOESN'T PROPERLY DISPLAY ON SCREEN SO
+//  I FORCEFULLY RELOAD THE PAGE. THIS NEEDS TO BE LOOKED INTO, SINCE IT MIGHT BE BETTER TO JUST
+//  UPDATE THE RECORDINGLIST, BUT THAT MIGHT IMPLY REWRITING THE WAY RECORDINGLIST WORKS ATM :(
+
+
+const ListAllRecordings = () => {
+
+  const { getCurrentUser } = useAppContext();
+  const [userData, setUserData] = useState(null);
+  const [recordingList, setRecordingList] = useState(null);
+  const [recordingNames, setRecordingNames] = useState(null);
+  const [recordingStars, setRecordingStars] = useState(null);
+  const [recordingScores, setRecordingScores] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+
+  // Starting --> load recordings from userID and scoreID
+  useEffect(() => {
+
+    const fetchDataFromAPI = () => {
+      if(userData===null){
+      getCurrentUser() // fetchData is already an async function
+        .then((result) => {
+          //console.log(`getCurentUser() has returnd this result: ${JSON.stringify(result)}`);
+          setUserData(result);
+        }).catch((error) => {
+          console.log(`getCurentUser() error: ${error}`)
+          // Handle errors if necessary
+        })
+      }
+
+      if(userData !== null){
+        getAllRecData("645b6e484612a8ebe8525933").then((result) => {
+          setRecordingList(JSON.stringify(result));
+          setRecordingNames(result.map((recording) => recording.recordingName));
+          setRecordingStars(result.map((recording) => recording.recordingStars));
+          setRecordingScores(result.map((recording) => recording.scoreID));
+        }).catch((error) => {
+          console.log(`Cannot get recordings from database: ${error}`)
+          // Handle errors if necessary
+        })
+
+      }
+  };   
+
+    fetchDataFromAPI();
+    
+  }, [userData, recordingList]);
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Event handler for going back
+  const handleGoBack = () => {
+    // Use navigate to go back to the previous page
+    //navigate(-1);
+    navigate(`/`);
+  };
+
+  // Event handler for click on See
+  const handleSeeClick = (nameOfFile, number)=> {
+      const id = JSON.parse(recordingList)[recordingNames.indexOf(nameOfFile)].recordingId;
+      //Pass recording ID to ProgressPlayfileVisual
+      //navigate(score, {state:{'recordingID':id}})
+    }
+
+
+  // Event handler for click on Trash
+  const handleTrashClick = (nameOfFile, number) => {
+    /*if (recordingNames.indexOf(nameOfFile) !== -1) {
+      const idToDelete = JSON.parse(recordingList)[recordingNames.indexOf(nameOfFile)].recordingId;
+      const auxArrayNames = recordingNames.filter((item, index) => index !== recordingNames.indexOf(nameOfFile));
+      const auxArrayList = JSON.parse(recordingList).filter((item, index) => index !== recordingNames.indexOf(nameOfFile));
+      console.log("Deleting: ", idToDelete);
+      deleteRecording(idToDelete).then(() => {
+        setRecordingNames(auxArrayNames);
+        setRecordingList(auxArrayList);
+        //window.location.reload();
+      }).catch((error) => {
+        console.log(`Cannot delete recordings from database: ${error}`)
+      })
+    }*/
+  };
+
+
+
+  if (recordingNames === null) {
+    return <p>Loading...</p>;
+  }
+
+
+
+  // Your component logic using the variables
+  return (
+    <div className={ListAllRecordingsCSS.container}>
+      <div> 
+        <h2>Your recordings...</h2>
+      </div>
+
+      {/* List of songs */}
+      <div className={ListAllRecordingsCSS.songlist}>
+        {recordingNames.map((nameOfFile, index) => (
+          <div className={ListAllRecordingsCSS.songelement} key={index}>
+          <li key={index}>
+              <div>{nameOfFile} - {recordingScores[index]}</div>
+              <div>
+              <button className={ListAllRecordingsCSS.iconbutton} onClick={() => handleSeeClick(nameOfFile, index)}>
+                <FontAwesomeIcon icon={faEye} />
+              </button>
+              <button className={ListAllRecordingsCSS.iconbutton} onClick={() => handleTrashClick(nameOfFile, index)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+              </div>
+              <div>
+                        <FontAwesomeIcon icon={faStar} className={recordingStars[index]>=1 ? ListAllRecordingsCSS.completeStar : ListAllRecordingsCSS.incompleteStar}/>
+                        <FontAwesomeIcon icon={faStar} className={recordingStars[index]>=2 ? ListAllRecordingsCSS.completeStar : ListAllRecordingsCSS.incompleteStar}/>
+                        <FontAwesomeIcon icon={faStar} className={recordingStars[index]>=3 ? ListAllRecordingsCSS.completeStar : ListAllRecordingsCSS.incompleteStar}/>
+                      </div>
+            </li>
+            </div>
+        ))}
+      </div>
+
+      {/* Button to go back */}
+      <button className={ListAllRecordingsCSS.backbutton} onClick={handleGoBack}>
+        Back
+      </button>
+    </div>
+  );
+};
+
+export default ListAllRecordings;
--- client-jb/src/utils/studentRecordingMethods.js
+++ client-jb/src/utils/studentRecordingMethods.js
@@ -19,6 +19,25 @@ async function getRecData(studentId, scoreId) {
     }
 }
 
+/* getAllRecData  --------------
+    return: res.body = [{recordingName, recordingId}, {...}, ...] 
+*/
+async function getAllRecData(studentId) {
+    try {
+        // scoreId: "64d0de60d9ac9a34a66b4d45" is for the score "V_001_Cuerdas_Al_Aire_1_Suelta_A"
+        const response = await axios.get("/api/v1/recordings/getAllRecData", {params: {studentId: studentId}});
+
+        if (response.status===200) {
+            return response.data; // save results locally
+        } else {
+            console.log('geAllRecData response.status is not 200!')
+            return [];
+        }
+    } catch (error) {
+        console.error('Error on axios getAllRecData', error);
+    }
+}
+
 /* putRecording  --------------
     object with all fields including audio for studentRecording model
     returns: {recordingName, recordingId}
@@ -96,4 +115,4 @@ async function deleteRecording(recordingId) {
     }
 }
 
-export { getRecData, getRecording, putRecording, deleteRecording, patchViewPermissions };
+export { getRecData, getAllRecData, getRecording, putRecording, deleteRecording, patchViewPermissions };
