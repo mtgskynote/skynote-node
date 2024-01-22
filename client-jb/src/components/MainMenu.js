d67b408c12b0f07790a82e32d86f83a37bc9eac9
Amaia <amaiasagastim@gmail.com>
2024-01-22 12:58:56 +0100

    menu hides when click outside

--- client-jb/src/components/MainMenu.js
+++ client-jb/src/components/MainMenu.js
@@ -6,13 +6,30 @@ import { Button } from "react-bootstrap";
 
 const MainMenu = () => {
   const { logoutUser } = useAppContext();
+
+  //If user clicks anywhere outside the menu, close menu
+  const handleOutsideClick = (event) => {
+    const dropdownToggle = document.getElementById('dropdown-toggle');
+    const dropdownContent = document.getElementById('dropdown-content');
+    if (
+      dropdownToggle &&
+      dropdownContent &&
+      !dropdownToggle.contains(event.target) &&
+      !dropdownContent.contains(event.target) && dropdownToggle.checked===true
+    ) {
+      dropdownToggle.checked = false;
+    }
+  };
+
+  document.addEventListener('click', handleOutsideClick);
+
   return (
     <div className={MainMenuCSS.dropdown} position="absolute"> 
       <input type="checkbox" id="dropdown-toggle" className={MainMenuCSS.dropdown_toggle}></input>
       <label className={MainMenuCSS.dropbtn} for="dropdown-toggle">Menu</label>
       <div className={MainMenuCSS.dropdown_content}>
 
-      <div className="nav-links">
+      <div className="nav-links" id="dropdown-content">
       {links.map((link) => {
         const { text, path, id, icon } = link;
 
@@ -36,21 +53,21 @@ const MainMenu = () => {
       })}
     </div>
     <Button
-              variant="outline-primary"
-              onClick={logoutUser}
-              style={{
-                display: "flex",
-                width: "70px",
-                height: "30px",
-                borderRadius: "10%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                float: "right",
-              }}
-            >
-              Logout
-            </Button>
+      variant="outline-primary"
+      onClick={logoutUser}
+      style={{
+        display: "flex",
+        width: "70px",
+        height: "30px",
+        borderRadius: "10%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        float: "right",
+      }}
+    >
+      Logout
+    </Button>
 
 
       </div>
