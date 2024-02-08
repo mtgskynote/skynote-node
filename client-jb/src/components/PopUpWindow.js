f7595af2ca2b7638ef17b04ec80335a59076b285
Alvaro Crespo <alvaro.crespo02@estudiant.upf.edu>
2024-02-08 10:00:51 +0100

    Dealt with some warnings :)

--- client-jb/src/components/PopUpWindow.js
+++ client-jb/src/components/PopUpWindow.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PopUpWindowCSS from './PopUpWindow.module.css';
 
 const PopUpWindow = (props) => {
-    const [showWindow, setShowWindow] = useState(false);
-    const [fileName, setFileName] = useState("");
 
-    useEffect(() => {
-        if(props.showWindow){
-            setShowWindow(true)
-        }else{
-            setShowWindow(false)
-        }
-      }, [props]);
+  const [fileName, setFileName] = useState("");
 
   const handleSave = () => {
     // Handle save logic here
     console.log("Saving recording")
     // Close/tell to hide the window after saving
-    setShowWindow(false); 
     props.handlerBack(false, "save", fileName)
   };
 
@@ -25,7 +16,6 @@ const PopUpWindow = (props) => {
     console.log("Deleting recording")
     // Handle delete logic here
     // Close/tell to hide the window after deleting
-    setShowWindow(false); 
     props.handlerBack(false, "delete")
   };
 
