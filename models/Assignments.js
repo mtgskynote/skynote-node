3c237177b8ce79c3b0298d9261acb6e2735958d8
Amaia <amaiasagastim@gmail.com>
2024-02-13 12:13:43 +0100

    assignments collection data model

---
+++ models/Assignments.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+
+const assignmentsSchema = new mongoose.Schema({
+    
+
+    teacherId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Reference to the Score model
+      },
+    students: [
+        {
+          studentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User', // Reference to the User model
+          },
+        },
+      ],
+    
+    message:  String,
+    post: Date,
+    due: Date,
+    tasks: [
+        { task:{
+            score: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Score', // Reference to the Score model
+            },
+            answers:[
+                {
+                    answer:{
+                        studentId: {
+                            type: mongoose.Schema.Types.ObjectId,
+                            ref: 'User', // Reference to the User model
+                        },
+                        recordingId: String,
+                        grade: Number,
+                        comment: String,
+
+                    }
+                }
+            ]
+        }   
+        },
+    ],
+
+})
+
+var assignments = mongoose.model('assignments', assignmentsSchema);
+
+export default assignments;
