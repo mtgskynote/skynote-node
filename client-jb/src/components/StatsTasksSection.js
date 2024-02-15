d26be427cb1cbb71b09e959039c7ca2ba044996f
Alvaro Crespo <alvaro.crespo02@estudiant.upf.edu>
2024-02-15 11:42:47 +0100

    Latest assignment is now displayed properly :)

--- client-jb/src/components/StatsTasksSection.js
+++ client-jb/src/components/StatsTasksSection.js
@@ -11,7 +11,7 @@ import {
     faTriangleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 
-const StatsTasksSection = (props) => {
+const StatsTasksSection = () => {
     const navigate = useNavigate();
     const { getCurrentUser } = useAppContext();
     const [userData, setUserData] = useState(null);
@@ -29,8 +29,7 @@ const StatsTasksSection = (props) => {
 
     const fetchAssignment = useCallback((userData) => {
       getLatestAssignment(userData.id).then((result)=>{
-        setAssignmentData(result)
-        console.log("Resultado", result[0]);
+        setAssignmentData(result[0])
       })
     }, [setAssignmentData]);
 
@@ -46,55 +45,62 @@ const StatsTasksSection = (props) => {
       if(userData!==null){
           fetchAssignment(userData);
       }
-    },[userData, fetchAssignment]);
+    },[userData]);
 
     /* FOR NOW THIS CODE DOES NOT DISPLAY ANYTHING REAL */
 
     // Event handler for click on OPEN
   const handleOpen = ()=> {
-    console.log("heyo")
     navigate(`/assignments/`)
   }
+
+  // Define options for formatting date
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
   
-  return (
-    <div className={StatsTasksCSS.container}>
-      <h4 className={StatsTasksCSS.title}>
-        Teacher announcement for this week 
-      </h4>
-    <div className={StatsTasksCSS.tableBox}> 
-        <div>
-            <div className={StatsTasksCSS.header}> 
-                <div> 
-                    Posted on : 23/01/2024 {/* REPLACE BY REAL DATE*/}
-                </div>
-                <div> 
-                    Due on : 30/01/2024{/* REPLACE BY REAL DATE*/}
-                </div>
-                <button className={StatsTasksCSS.openButton} onClick={() => handleOpen()}> 
-                    OPEN <FontAwesomeIcon icon={faFileImport} className={StatsTasksCSS.openIcon} />
-                </button>
-            </div>
-            <div className={StatsTasksCSS.announcementBody}> 
-                <div className={StatsTasksCSS.teacher}> 
-                    TEACHER   <FontAwesomeIcon icon={faUser} className={StatsTasksCSS.userIcon}/>  said...
-                </div>
-                <div className={StatsTasksCSS.message}> {/* REPLACE BY REAL MESSAGE*/}
-                    Hello User, for this week the assignments include:
-                    <ul className={StatsTasksCSS.list}>
-                    <li> Practice C major scales. We will have a look at it next week in class.</li>
-                    <li>I am opening a submission task for "The Fireman", make s...</li>
-                    </ul>
-                </div>
-                <div className={StatsTasksCSS.note}>
-                    <FontAwesomeIcon icon={faTriangleExclamation} className={StatsTasksCSS.exclamationIcon} />
-                     <div className={StatsTasksCSS.text}>2 submission assignments linked to this announcement</div> {/* REPLACE BY REAL number of assignments*/}
-                    <FontAwesomeIcon icon={faTriangleExclamation} className={StatsTasksCSS.exclamationIcon} />
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-  );
+  if (assignmentData!==null){
+    return (
+      <div className={StatsTasksCSS.container}>
+        <h4 className={StatsTasksCSS.title}>
+          Latest announcement 
+        </h4>
+      <div className={StatsTasksCSS.tableBox}> 
+          <div>
+              <div className={StatsTasksCSS.header}> 
+                  <div> 
+                      Posted on : {new Date(assignmentData.post).toLocaleDateString("es-ES", options)}
+                  </div>
+                  <div> 
+                      Due on : {new Date(assignmentData.due).toLocaleDateString("es-ES", options)}
+                  </div>
+                  <button className={StatsTasksCSS.openButton} onClick={() => handleOpen()}> 
+                      OPEN <FontAwesomeIcon icon={faFileImport} className={StatsTasksCSS.openIcon} />
+                  </button>
+              </div>
+              <div className={StatsTasksCSS.announcementBody}> 
+                  <div className={StatsTasksCSS.teacher}> 
+                      TEACHER   <FontAwesomeIcon icon={faUser} className={StatsTasksCSS.userIcon}/>  said...
+                  </div>
+                  <div className={StatsTasksCSS.message}> 
+                    {assignmentData.message}
+                  </div>
+                  <div className={StatsTasksCSS.note}>
+                      <FontAwesomeIcon icon={faTriangleExclamation} className={StatsTasksCSS.exclamationIcon} />
+                       <div className={StatsTasksCSS.text}>{assignmentData.tasks.length} submission assignments linked to this announcement</div>
+                      <FontAwesomeIcon icon={faTriangleExclamation} className={StatsTasksCSS.exclamationIcon} />
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
+    );
+  }
+  
 };
 
 export default StatsTasksSection;
