a7bd3e420686e309f30af4e0fce9cc0af528da8e
Alvaro Crespo <alvaro.crespo02@estudiant.upf.edu>
2024-03-21 10:16:13 +0100

    Removed wrapper from SharedLayout.js :)

--- client-jb/src/pages/dashboard/SharedLayout.js
+++ client-jb/src/pages/dashboard/SharedLayout.js
@@ -1,18 +1,16 @@
 import { Outlet } from "react-router-dom";
-import Wrapper from "../../assets/wrappers/SharedLayout";
 import MainMenu from "../../components/MainMenu";
+import SharedLayoutCSS from "./SharedLayout.module.css"
 
 const SharedLayout = () => {
   return (
-    <Wrapper>
-      <main className="dashboard">
-        <MainMenu ></MainMenu> 
-        {/* <Navbar /> */}
-        <div className="dashboard-page">
-          <Outlet />
-        </div>
-      </main>
-    </Wrapper>
+    <main className={SharedLayoutCSS.dashboard}>
+      <MainMenu ></MainMenu> 
+      {/* <Navbar /> */}
+      <div className={SharedLayoutCSS.dashboardPage}>
+        <Outlet />
+      </div>
+    </main>
   );
 };
 
