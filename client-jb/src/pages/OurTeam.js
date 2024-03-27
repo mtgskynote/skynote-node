078f3b08f71dbd24609476c3dbef7683a8e789a3
Alvaro Crespo <alvaro.crespo02@estudiant.upf.edu>
2024-03-27 12:49:25 +0100

    Created OurTeam.module.css :)

--- client-jb/src/pages/OurTeam.js
+++ client-jb/src/pages/OurTeam.js
@@ -5,65 +5,66 @@ import {
   import lonceimg from "../assets/images/team/lonce_cnm_bust_sm2.jpg";
   import rafaelimg from "../assets/images/team/RafaelRamirez.png";
 import Wrapper from "../assets/wrappers/LandingBackPages";
+import OurTeamCSS from './OurTeam.module.css'
 import WhiteLogo from "../components/WhiteLogo";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 const OurTeam = () => {
     return (
       <Wrapper>
-        <div className="site-header">
-          <div className="tab-row-container">
-            <div className="tab-row">
-            <div className="tab-login"><Link to="/landing" className="tab-link"> Home </Link></div>
-              <div className="tab-login"><Link to="/demos" className="tab-link"> Demos </Link></div>
-              <div className="tab-login"><Link to="/ourteam" className="tab-link"> Our Team </Link></div>
-              <div className="tab-login"><Link to="/research" className="tab-link"> Research </Link></div>
-              <div className="tab-login"><Link to="https://docs.google.com/forms/d/e/1FAIpQLSdE6QIt2Xfno67jWjBi2SJOB1dImKhmvJYr9Mzi9Qbo1BGHuw/viewform" className="tab-link"> Interest Form </Link></div>
-              <div className="tab-login"><Link to="/register" className="tab-link"> Login Register </Link></div>
+        <div className={OurTeamCSS.siteHeader}>
+          <div className={OurTeamCSS.tabRowContainer}>
+            <div className={OurTeamCSS.tabRow}>
+            <div className={OurTeamCSS.tabLogin}><Link to="/landing" className={OurTeamCSS.tabLink}> Home </Link></div>
+              <div className={OurTeamCSS.tabLogin}><Link to="/demos" className={OurTeamCSS.tabLink}> Demos </Link></div>
+              <div className={OurTeamCSS.tabLogin}><Link to="/ourteam" className={OurTeamCSS.tabLink}> Our Team </Link></div>
+              <div className={OurTeamCSS.tabLogin}><Link to="/research" className={OurTeamCSS.tabLink}> Research </Link></div>
+              <div className={OurTeamCSS.tabLogin}><Link to="https://docs.google.com/forms/d/e/1FAIpQLSdE6QIt2Xfno67jWjBi2SJOB1dImKhmvJYr9Mzi9Qbo1BGHuw/viewform" className={OurTeamCSS.tabLink}> Interest Form </Link></div>
+              <div className={OurTeamCSS.tabLogin}><Link to="/register" className={OurTeamCSS.tabLink}> Login Register </Link></div>
             </div>
 
 
-          <div className="dropdown">
-            <input type="checkbox" id="dropdown-toggle" className="dropdown-toggle"></input>
-            <label className="dropbtn" htmlFor="dropdown-toggle">Menu</label>
-            <div className="dropdown-content">
+          <div className={OurTeamCSS.dropdown}>
+            <input type="checkbox" id="dropdown-toggle" className={OurTeamCSS.dropdownToggle}></input>
+            <label className={OurTeamCSS.dropbtn} htmlFor="dropdown-toggle">Menu</label>
+            <div className={OurTeamCSS.dropdownContent}>
               <ul>
-                <li><Link to="/landing" className="tab-link"> Home </Link></li>
-                <li><Link to="/demos" className="tab-link"> Demos </Link></li>
-                <li><Link to="/ourteam" className="tab-link"> Our Team </Link></li>
-                <li><Link to="/research" className="tab-link"> Research </Link></li>
-                <li><Link to="https://docs.google.com/forms/d/e/1FAIpQLSdE6QIt2Xfno67jWjBi2SJOB1dImKhmvJYr9Mzi9Qbo1BGHuw/viewform" className="tab-link"> Interest Form </Link></li>
-                <li><Link to="/register" className="tab-link"> Login Register </Link></li>
+                <li><Link to="/landing" className={OurTeamCSS.tabLink}> Home </Link></li>
+                <li><Link to="/demos" className={OurTeamCSS.tabLink}> Demos </Link></li>
+                <li><Link to="/ourteam" className={OurTeamCSS.tabLink}> Our Team </Link></li>
+                <li><Link to="/research" className={OurTeamCSS.tabLink}> Research </Link></li>
+                <li><Link to="https://docs.google.com/forms/d/e/1FAIpQLSdE6QIt2Xfno67jWjBi2SJOB1dImKhmvJYr9Mzi9Qbo1BGHuw/viewform" className={OurTeamCSS.tabLink}> Interest Form </Link></li>
+                <li><Link to="/register" className={OurTeamCSS.tabLink}> Login Register </Link></li>
               </ul>
             </div>
           </div> 
 
 
           </div>
-            <div className="logo-container"> 
+            <div className={OurTeamCSS.logoContainer}> 
               <WhiteLogo/>
             </div>
-            <div className="skynote-container">
+            <div className={OurTeamCSS.skynoteContainer}>
               SkyNote
             </div>
         </div>
-        <div className="addspace1"></div>
-         <div className="work-section-top">
-         <div className="top-p-container">
+        <div className={OurTeamCSS.addspace1}></div>
+         <div className={OurTeamCSS.workSectionTop}>
+         <div className={OurTeamCSS.topPContainer}>
         <h1> Our Team</h1>
         <div>
           <br></br>
         </div>
-        <h2 className="primary-subheading">
+        <h2>
           We are a team of researchers and developers, 
           experts in music and machine learning
         </h2>
         </div>
         </div>
-        <div className="row">
-          <div className="column">
-            <div className="card">
-              <div className="img-container">
+        <div className={OurTeamCSS.row}>
+          <div className={OurTeamCSS.column}>
+            <div className={OurTeamCSS.card}>
+              <div className={OurTeamCSS.imgContainer}>
                 <img src={lonceimg} alt="Write description here"/>
               </div>
               <h3>Dr. Lonce Wyse</h3>
@@ -77,7 +78,7 @@ const OurTeam = () => {
               real-time musical  communication and notation, and audio technologies
               for music education.
               </p>
-              <div className="icons">
+              <div className={OurTeamCSS.icons}>
                 <a href="https://www.linkedin.com/in/lonce/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedinIn />
                 </a>
@@ -85,9 +86,9 @@ const OurTeam = () => {
             </div>
           </div> 
 
-          <div className="column">
-            <div className="card">
-              <div className="img-container">
+          <div className={OurTeamCSS.column}>
+            <div className={OurTeamCSS.card}>
+              <div className={OurTeamCSS.imgContainer}>
                 <img src={rafaelimg} alt="Write description here"/>
               </div>
               <h3>Dr. Rafael Ramirez</h3>
@@ -99,7 +100,7 @@ const OurTeam = () => {
               am passionate about Artificial Intelligence and Music, and how they can
               help improving people's lives.
               </p>
-              <div className="icons">
+              <div className={OurTeamCSS.icons}>
                 <a href="https://www.linkedin.com/in/rafaelr2/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedinIn />
                 </a>
@@ -107,16 +108,16 @@ const OurTeam = () => {
             </div>
           </div>
 
-          <div className="column">
-            <div className="card">
-              <div className="img-container">
+          <div className={OurTeamCSS.column}>
+            <div className={OurTeamCSS.card}>
+              <div className={OurTeamCSS.imgContainer}>
                 <img src={waddellimg} alt="Write description here"/>
               </div>
               <h3>Dr.George Waddell</h3>
               <p>
               Dr. George Waddell is Performance Research and Innovation Fellow and BMus Area Leader in Performance Science at the Royal College of Music. He is also a Module Leader and Honorary Research Associate in the Faculty of Medicine at Imperial College London. His research focusses on the evaluation of performance, including decision-making among judges and audiences and the development of evaluative skills, as well as how technology can be used to feed analytics data back to performers to enhance their practice.
               </p>
-              <div className="icons">
+              <div className={OurTeamCSS.icons}>
                 <a href="https://www.linkedin.com/in/drgeorgewaddell/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedinIn />
                 </a>
