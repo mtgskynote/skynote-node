85d4cee925c09154b98e6367878b237f27af3ec0
Isabelle Oktay <iso212@nyu.edu>
2024-06-04 10:49:23 +0200

    Change available icons in control bar

--- client-jb/src/components/ControlBarAlt.js
+++ client-jb/src/components/ControlBarAlt.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import ModeToggleAlt from "./ModeToggleAlt";
 import {
-  PlayCircle as PlayCircleIcon,
-  PauseCircle as PauseCircleIcon,
+  PlayCircle as PlayIcon,
+  PauseCircle as PauseIcon,
   RadioButtonChecked as RecordIcon,
   ImportExport as TransposeIcon,
   AccessTime as BpmIcon,
   VolumeUp as VolumeIcon,
+  Hearing as HearingIcon,
 } from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
@@ -14,12 +15,17 @@ import Tooltip from "@mui/material/Tooltip";
 const ControlBarAlt = () => {
   const [practiceModeOn, setPracticeModeOn] = useState(true);
   const allModeIcons = [
-    { tooltip: "Play", icon: <PlayCircleIcon className="text-4xl" /> },
     { tooltip: "Transpose", icon: <TransposeIcon className="text-4xl" /> },
     { tooltip: "BPM", icon: <BpmIcon className="text-4xl" /> },
     { tooltip: "Volume", icon: <VolumeIcon className="text-4xl" /> },
   ];
-  console.log(practiceModeOn);
+  const practiceModeIcons = [
+    { tooltip: "Listen", icon: <HearingIcon className="text-4xl" /> },
+    { tooltip: "Practice", icon: <PlayIcon className="text-4xl" /> },
+  ];
+  const recordModeIcons = [
+    { tooltip: "Record", icon: <RecordIcon className="text-4xl" /> },
+  ];
 
   return (
     <div className="px-4 py-3 bg-blue-400 rounded-3xl shadow-md w-1/2">
@@ -33,15 +39,21 @@ const ControlBarAlt = () => {
         <div className="ml-6 h-full w-0.5 self-stretch bg-neutral-100 dark:bg-white/10"></div>
 
         <div className="flex flex-grow justify-around">
-          {!practiceModeOn && (
-            <div className="transition-all duration-500 ease-in-out">
-              <Tooltip title="Record">
-                <IconButton className="text-white">
-                  <RecordIcon className="text-4xl" />
-                </IconButton>
-              </Tooltip>
-            </div>
-          )}
+          {practiceModeOn
+            ? practiceModeIcons.map((modeIcon, index) => (
+                <Tooltip title={modeIcon.tooltip} key={index}>
+                  <IconButton className="text-white">
+                    {modeIcon.icon}
+                  </IconButton>
+                </Tooltip>
+              ))
+            : recordModeIcons.map((modeIcon, index) => (
+                <Tooltip title={modeIcon.tooltip} key={index}>
+                  <IconButton className="text-white">
+                    {modeIcon.icon}
+                  </IconButton>
+                </Tooltip>
+              ))}
           {allModeIcons.map((modeIcon, index) => (
             <Tooltip title={modeIcon.tooltip} key={index}>
               <IconButton className="text-white">{modeIcon.icon}</IconButton>
