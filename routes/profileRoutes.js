78da519963ef8220709aadc945add75ca5a9804d
Isabelle Oktay <iso212@nyu.edu>
2024-06-25 12:44:33 +0200

    Logout user whenever JWT token is removed from local storage or expires

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,15 +1,21 @@
-import express from 'express';
-import { changePassword, updateProfileData } from '../controllers/profileController.js';
-import authenticateUser from "../middleware-jb/authenticateUser.js";
+import express from "express";
+import {
+  changePassword,
+  updateProfileData,
+} from "../controllers/profileController.js";
+import { authenticateUser } from "../middleware-jb/authenticateUser.js";
 
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 // Middleware setup for parsing form data
 const formParser = bodyParser.urlencoded({ extended: false });
 
 const router = express.Router();
 
-router.route("/changePassword").post(authenticateUser, formParser, changePassword);
-router.route("/updateProfileData").post(authenticateUser, formParser, updateProfileData);
-
+router
+  .route("/changePassword")
+  .post(authenticateUser, formParser, changePassword);
+router
+  .route("/updateProfileData")
+  .post(authenticateUser, formParser, updateProfileData);
 
 export default router;
