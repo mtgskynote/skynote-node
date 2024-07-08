a25f7c31c46325db9df937caebc6b6da909163e5
Isabelle Oktay <iso212@nyu.edu>
2024-07-08 12:31:31 +0200

    Finish new List Recordings view and functionality

--- client-jb/src/components/ListRecordings.js
+++ client-jb/src/components/ListRecordings.js
@@ -1,23 +1,8 @@
 // ListRecordings.js
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import ListRecordingsCSS from "./ListRecordings.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  getRecData,
-  deleteRecording,
-} from "../utils/studentRecordingMethods.js";
+import { getRecData } from "../utils/studentRecordingMethods.js";
 import { useAppContext } from "../context/appContext";
-import {
-  faTrash,
-  faEye,
-  faStar,
-  faPencilSquare,
-  faBoxArchive,
-  faMusic,
-  faPenToSquare,
-} from "@fortawesome/free-solid-svg-icons";
-import PopUpWindowEdit from "./PopUpWindowEdit.js";
 import LoadingScreen from "./LoadingScreen.js";
 import RecordingCard from "./RecordingCard.js";
 
@@ -32,14 +17,10 @@ const ListRecordings = () => {
   const [scoreSkill, setScoreSkill] = useState(null);
   const [scoreLevel, setScoreLevel] = useState(null);
   const [scoreXml, setScoreXml] = useState(null);
-  const [idSelectedEdit, setIdSelectedEdit] = useState(null);
-  const [showPopUpEdit, setShowPopUpEdit] = useState(false);
-
-  const [showDeleteBanner, setShowDeleteBanner] = useState(false);
-  const [deleteMessage, setDeleteMessage] = useState("");
 
   const location = useLocation();
   const navigate = useNavigate();
+
   // Define options for formatting date
   const options = {
     year: "numeric",
@@ -53,7 +34,7 @@ const ListRecordings = () => {
   const score = location.state?.score || "DefaultSong";
   const song = location.state?.song || "DefaultSong1";
 
-  // Starting --> load recordings from userID and scoreID
+  // Load recordings from userID and scoreID
   useEffect(() => {
     const itemFoundLocalStorage = JSON.parse(
       localStorage.getItem("scoreData")
@@ -85,9 +66,9 @@ const ListRecordings = () => {
             );
             setRecordingDates(
               result.map((recording) => {
-                //Set correct date format
+                // Set correct date format
                 const recordingDate = new Date(recording.recordingDate);
-                return recordingDate.toLocaleDateString("es-ES", options);
+                return recordingDate.toLocaleDateString("en-UK", options);
               })
             );
           })
@@ -100,65 +81,34 @@ const ListRecordings = () => {
     fetchDataFromAPI();
   }, [userData]);
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Event handler for going back
+  // Go back to previous page
   const handleGoBack = () => {
-    // Use navigate to go back to the previous page
-    //navigate(-1);
-    navigate(`/all-lessons/${score}`);
+    navigate(-1);
   };
 
-  // Event handler for click on See
-  const handleSeeClick = (nameOfFile, number) => {
-    const recording = recordingList[recordingNames.indexOf(nameOfFile)];
-    //Pass recording ID to ProgressPlayfileVisual
-    navigate(score, { state: { id: recording.recordingId } });
-  };
-
-  // Event handler for click on Edit
-  const handleEditClick = (action, nameOfFile) => {
-    if (action === "open") {
-      const id = recordingList[recordingNames.indexOf(nameOfFile)].recordingId;
-      //Store id to edit so that popupwindow can access it
-      setIdSelectedEdit(id);
-      // Show pop up window component
-      setShowPopUpEdit(true);
-    } else {
-      // Dont show pop up window component
-      setShowPopUpEdit(false);
-      //Delete stored id
-      setIdSelectedEdit(null);
-    }
-  };
-
-  // Handle delete recording by
-  const handleDeleteRecording = (recordingName, recordingId) => {
+  // Update state variables after deleting recording
+  const handleDeleteRecording = (recordingName) => {
     const index = recordingNames.indexOf(recordingName);
     if (index !== -1) {
       const newRecordingNames = recordingNames.filter((_, i) => i !== index);
       const newRecordingList = recordingList.filter((_, i) => i !== index);
       const newRecordingDates = recordingDates.filter((_, i) => i !== index);
       const newRecordingIds = recordingIds.filter((_, i) => i !== index);
-      deleteRecording(recordingId)
-        .then(() => {
-          setDeleteMessage(`Successfully deleted ${recordingName}.`);
-          setShowDeleteBanner(true); // Show delete banner
-          setTimeout(() => {
-            setShowDeleteBanner(false); // Hide delete banner after 3 seconds
-          }, 3000); // Adjust timing as needed
-          setRecordingNames(newRecordingNames);
-          setRecordingList(newRecordingList);
-          setRecordingDates(newRecordingDates);
-          setRecordingIds(newRecordingIds);
-        })
-        .catch((error) => {
-          console.log(`Cannot delete recording from database: ${error}`);
-          setDeleteMessage(`Could not delete ${recordingName}.`);
-          setShowDeleteBanner(true); // Show delete banner
-          setTimeout(() => {
-            setShowDeleteBanner(false); // Hide delete banner after 3 seconds
-          }, 3000); // Adjust timing as needed
-        });
+
+      setRecordingNames(newRecordingNames);
+      setRecordingList(newRecordingList);
+      setRecordingDates(newRecordingDates);
+      setRecordingIds(newRecordingIds);
+    }
+  };
+
+  // Update state variables after editing recording
+  const handleEditRecording = (recordingName, newRecordingName) => {
+    const index = recordingNames.indexOf(recordingName);
+    if (index !== -1) {
+      const updatedRecordingNames = [...recordingNames];
+      updatedRecordingNames[index] = newRecordingName;
+      setRecordingNames(updatedRecordingNames);
     }
   };
 
@@ -166,7 +116,6 @@ const ListRecordings = () => {
     return <LoadingScreen />;
   }
 
-  // Your component logic using the variables
   return (
     <div className="container mx-auto px-4 py-8">
       <button
@@ -187,15 +136,17 @@ const ListRecordings = () => {
       </div>
       <hr className="h-0.5 border-t-0 bg-gray-700 mb-10" />
       {recordingNames.length !== 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
           {recordingNames.map((recordingName, index) => (
             <div key={index} className="flex justify-center">
               <RecordingCard
                 recordingName={recordingName}
                 stars={recordingStars[index]}
                 recordingId={recordingIds[index]}
+                recordingDate={recordingDates[index]}
                 xml={scoreXml}
                 onDeleteRecording={handleDeleteRecording}
+                onEditRecording={handleEditRecording}
               />
             </div>
           ))}
@@ -203,121 +154,7 @@ const ListRecordings = () => {
       ) : (
         <p className="w-full">No recordings found.</p>
       )}
-      <div
-        className={`fixed bottom-0 left-0 right-0 bg-green-500 text-white p-3 flex items-center justify-center transition-transform transform ${
-          showDeleteBanner ? "translate-y-0" : "translate-y-full"
-        }`}
-      >
-        <p>{deleteMessage}</p>
-      </div>
     </div>
-    // <div className={ListRecordingsCSS.container}>
-    //   <h2>
-    //     <FontAwesomeIcon icon={faMusic} className={ListRecordingsCSS.auxIcon} />
-    //     {song}
-    //     <FontAwesomeIcon icon={faMusic} className={ListRecordingsCSS.auxIcon} />
-    //   </h2>
-    //   <div className={ListRecordingsCSS.textGroup}>
-    //     <div>
-    //       <h6>
-    //         <FontAwesomeIcon
-    //           icon={faPencilSquare}
-    //           className={ListRecordingsCSS.auxIcon}
-    //         />
-    //         {scoreSkill}
-    //       </h6>
-    //     </div>
-    //     <div>
-    //       <h6>
-    //         <FontAwesomeIcon
-    //           icon={faBoxArchive}
-    //           className={ListRecordingsCSS.auxIcon}
-    //         />
-    //         Level {scoreLevel}
-    //       </h6>
-    //     </div>
-    //   </div>
-
-    //   {/* List of songs */}
-    //   {recordingNames.length !== 0 ? (
-    //     <div className={ListRecordingsCSS.songlist}>
-    //       {recordingNames.map((nameOfFile, index) => (
-    //         <div className={ListRecordingsCSS.songelement} key={index}>
-    //           <li key={index}>
-    //             <div className={ListRecordingsCSS.recTitle}>
-    //               <h5>
-    //                 {nameOfFile}{" "}
-    //                 <FontAwesomeIcon
-    //                   icon={faPenToSquare}
-    //                   className={ListRecordingsCSS.iconModify}
-    //                   onClick={() => handleEditClick("open", nameOfFile)}
-    //                 />{" "}
-    //               </h5>
-    //             </div>
-    //             <div className={ListRecordingsCSS.starsGroup}>
-    //               <FontAwesomeIcon
-    //                 icon={faStar}
-    //                 className={
-    //                   recordingStars[index] >= 1
-    //                     ? ListRecordingsCSS.completeStar
-    //                     : ListRecordingsCSS.incompleteStar
-    //                 }
-    //               />
-    //               <FontAwesomeIcon
-    //                 icon={faStar}
-    //                 className={
-    //                   recordingStars[index] >= 2
-    //                     ? ListRecordingsCSS.completeStar
-    //                     : ListRecordingsCSS.incompleteStar
-    //                 }
-    //               />
-    //               <FontAwesomeIcon
-    //                 icon={faStar}
-    //                 className={
-    //                   recordingStars[index] >= 3
-    //                     ? ListRecordingsCSS.completeStar
-    //                     : ListRecordingsCSS.incompleteStar
-    //                 }
-    //               />
-    //             </div>
-    //             <div>
-    //               <button
-    //                 className={ListRecordingsCSS.iconbutton}
-    //                 onClick={() => handleSeeClick(nameOfFile, index)}
-    //               >
-    //                 <FontAwesomeIcon icon={faEye} />
-    //               </button>
-    //               <button
-    //                 className={ListRecordingsCSS.iconbutton}
-    //                 onClick={() => handleTrashClick(nameOfFile, index)}
-    //               >
-    //                 <FontAwesomeIcon icon={faTrash} />
-    //               </button>
-    //             </div>
-    //             <div className={ListRecordingsCSS.dateTime}>
-    //               <i>{recordingDates[index]}</i>
-    //             </div>
-    //           </li>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   ) : (
-    //     <div> No recordings for this score</div>
-    //   )}
-
-    //   {/* Button to go back */}
-    //   <button className={ListRecordingsCSS.backbutton} onClick={handleGoBack}>
-    //     Back
-    //   </button>
-    //   {showPopUpEdit ? (
-    //     <PopUpWindowEdit
-    //       idEdit={idSelectedEdit}
-    //       handlerBack={handleEditClick}
-    //     />
-    //   ) : (
-    //     ""
-    //   )}
-    // </div>
   );
 };
 
--- client-jb/src/components/RecordingCard.js
+++ client-jb/src/components/RecordingCard.js
@@ -1,27 +1,45 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { deleteRecording } from "../utils/studentRecordingMethods";
-import { CardContent, Typography, Tooltip, IconButton } from "@mui/material";
+import {
+  deleteRecording,
+  editRecording,
+} from "../utils/studentRecordingMethods";
+import {
+  CardContent,
+  Typography,
+  Tooltip,
+  IconButton,
+  CircularProgress,
+} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import StarRating from "./StarRating";
+import PopUpWindow from "./PopUpWindow";
 
 const RecordingCard = ({
   recordingName,
   skill,
   stars,
   xml,
-  id,
   recordingId,
+  recordingDate,
   onDeleteRecording,
   width,
   backgroundColour,
   hoverBackgroundColour,
   textColour,
+  onEditRecording,
 }) => {
+  const [showEditPopUpWindow, setShowEditPopUpWindow] = useState(false);
+  const [showDeletePopUpWindow, setShowDeletePopUpWindow] = useState(false);
+  const [newRecordingName, setNewRecordingName] = useState(recordingName);
+  const [showEditWarning, setShowEditWarning] = useState(false);
+  const [showDeleteWarning, setShowDeleteWarning] = useState(false);
+  const [showEditLoading, setShowEditLoading] = useState(false);
+  const [showDeleteLoading, setShowDeleteLoading] = useState(false);
   const navigate = useNavigate();
 
-  // set defaults
+  // Set defaults
   const xSize = width ? width : "290px";
   const bColour = backgroundColour ? backgroundColour : "bg-blue-400";
   const hoverColour = hoverBackgroundColour
@@ -29,9 +47,73 @@ const RecordingCard = ({
     : "hover:bg-blue-500";
   const tColour = textColour ? textColour : "text-white";
 
-  // Navigates to the ListRecordings route with the provided xml and recordingId.
+  // Navigate to the ListRecordings route with the provided xml and recordingId
   const handleViewRecording = async () => {
     navigate(`/ListRecordings/${xml}`, { state: { id: recordingId } });
+  };
+
+  // Handle opening the delete recording popup window
+  const handleShowEditPopUpWindow = () => {
+    setShowEditPopUpWindow(true);
+  };
+
+  // Handle closing the delete recording popup window
+  const handleHideEditPopUpWindow = () => {
+    setShowEditPopUpWindow(false);
+    setTimeout(() => {
+      setNewRecordingName(recordingName);
+      setShowEditWarning(false);
+    }, 1000);
+  };
+
+  // Set new recording name whenever the user input changes
+  const handleRenameRecording = (e) => {
+    setNewRecordingName(e.target.value);
+  };
+
+  // Update recording name in the database
+  const handleUpdateRecordingName = () => {
+    if (newRecordingName !== "" && newRecordingName !== recordingName) {
+      setShowEditWarning(false);
+      setShowEditLoading(true);
+      editRecording(recordingId, newRecordingName).then(() => {
+        onEditRecording(recordingName, newRecordingName);
+        setNewRecordingName(newRecordingName);
+        setShowEditPopUpWindow(false);
+        setShowEditLoading(false);
+      });
+    } else if (newRecordingName === recordingName) {
+      setShowEditWarning(false);
+      setShowEditPopUpWindow(false);
+    } else {
+      setShowEditWarning(true);
+    }
+  };
+
+  // Open the delete recording popup window
+  const handleShowDeletePopUpWindow = () => {
+    setShowDeletePopUpWindow(true);
+  };
+
+  // Close the delete recording popup window
+  const handleHideDeletePopUpWindow = () => {
+    setShowDeletePopUpWindow(false);
+  };
+
+  // Delete recording from the database
+  const handleDeleteRecording = () => {
+    setShowDeleteLoading(true);
+    deleteRecording(recordingId)
+      .then(() => {
+        onDeleteRecording(recordingName);
+        setShowDeletePopUpWindow(false);
+        setShowDeleteLoading(false);
+      })
+      .catch((error) => {
+        console.log(`Cannot delete recording from database: ${error}`);
+        setShowDeleteLoading(false);
+        setShowDeleteWarning(true);
+      });
   };
 
   return (
@@ -41,10 +123,8 @@ const RecordingCard = ({
           className={`relative transition ease-in-out delay-50 rounded overflow-hidden shadow-md hover:shadow-lg ${bColour} ${hoverColour} hover:cursor-pointer mb-3`}
           style={{ width: xSize, aspectRatio: "3 / 2" }}
           onClick={handleViewRecording}
-          id={id}
         >
           <CardContent>
-            {/* Top Section */}
             <div
               className={`flex justify-between items-start ${tColour} align-text-top`}
             >
@@ -58,7 +138,11 @@ const RecordingCard = ({
                 </Typography>
               </div>
               <div></div>
-              <div></div>
+              <div
+                className={`sm:text-xs md:text-xs lg:text-xs xl:text-sm font-extralight p-1 rounded align-right`}
+              >
+                {recordingDate}
+              </div>
             </div>
 
             <div className="whitespace-normal w-fit">
@@ -78,7 +162,13 @@ const RecordingCard = ({
               />
               <div className="grid grid-cols-2 ml-auto">
                 <Tooltip placement="bottom" title="Edit" arrow>
-                  <IconButton className="text-white">
+                  <IconButton
+                    className="text-white"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleShowEditPopUpWindow();
+                    }}
+                  >
                     <EditIcon className="text-3xl" />
                   </IconButton>
                 </Tooltip>
@@ -87,7 +177,7 @@ const RecordingCard = ({
                     className="text-white"
                     onClick={(e) => {
                       e.stopPropagation();
-                      onDeleteRecording(recordingName, recordingId);
+                      handleShowDeletePopUpWindow();
                     }}
                   >
                     <DeleteIcon className="text-3xl" />
@@ -98,6 +188,91 @@ const RecordingCard = ({
           </CardContent>
         </div>
       </div>
+
+      {/* Edit Pop Up Window */}
+      <PopUpWindow isOpen={showEditPopUpWindow}>
+        <div className="relative">
+          {showEditLoading && (
+            <div className="absolute inset-0 bg-white bg-opacity-75 flex justify-center items-center z-10">
+              <CircularProgress />
+            </div>
+          )}
+          <div className={`mb-4 ${showEditLoading ? "opacity-50" : ""}`}>
+            <label className="block text-gray-700 mb-2" htmlFor="recordingName">
+              Edit recording name:
+            </label>
+            <input
+              type="text"
+              id="recordingName"
+              value={newRecordingName}
+              onChange={handleRenameRecording}
+              className="w-full px-3 py-2 mb-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              disabled={showEditLoading}
+            />
+            {showEditWarning && (
+              <div className="bg-red-100 text-red-700 text-sm py-1 px-2 mb-4 rounded-lg flex justify-center items-center transition duration-300 ease-in-out">
+                <p className="m-0">Recording name cannot be empty</p>
+              </div>
+            )}
+          </div>
+          <div className="flex justify-between space-x-2">
+            <button
+              onClick={handleHideEditPopUpWindow}
+              className="bg-slate-50 border-solid border-slate-500 outline-none text-slate-500 px-4 py-2 rounded-lg hover:bg-slate-100 transition duration-300 ease-in-out w-full"
+              disabled={showEditLoading}
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleUpdateRecordingName}
+              className="bg-green-500 border-none outline-none text-white px-4 py-2 rounded-lg hover:bg-green-600 transition duration-300 ease-in-out w-full"
+              disabled={showEditLoading}
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </PopUpWindow>
+
+      {/* Delete Pop Up Window */}
+      <PopUpWindow isOpen={showDeletePopUpWindow}>
+        <div className="relative">
+          {showDeleteLoading && (
+            <div className="absolute inset-0 bg-white bg-opacity-75 flex justify-center items-center z-10">
+              <CircularProgress />
+            </div>
+          )}
+          <div className={`${showDeleteLoading ? "opacity-50" : ""}`}>
+            {showDeleteWarning && (
+              <div className="bg-red-100 text-red-700 text-sm py-1 px-2 mb-4 rounded-lg flex justify-center items-center transition duration-300 ease-in-out">
+                <p className="m-0">Could not delete {recordingName}.</p>
+              </div>
+            )}
+            <p className="text-xl font-extrabold text-gray-800 mb-1">
+              Are you sure you want to delete <span>{recordingName}</span>?
+            </p>
+            <p className="text-gray-600 mb-4">
+              This action is permanent and cannot be undone.
+            </p>
+            <div className="flex justify-between space-x-2">
+              <button
+                onClick={handleHideDeletePopUpWindow}
+                className="bg-slate-50 border-solid border-slate-500 outline-none text-slate-500 px-4 py-2 rounded-lg hover:bg-slate-100 transition duration-300 ease-in-out w-full"
+                disabled={showDeleteLoading}
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDeleteRecording}
+                className="bg-red-500 border-none outline-none text-white px-4 py-2 rounded-lg hover:bg-red-700 transition duration-300 ease-in-out w-full"
+                disabled={showDeleteLoading}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </PopUpWindow>
     </div>
   );
 };
