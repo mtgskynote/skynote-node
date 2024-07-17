4093944b8758327e000584a96fa6ec55f4df239b
Suvi Häärä <suvi.haara@hotmail.com>
2024-07-17 11:14:19 +0200

    [ENG-72]: prettier auto formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   pluginReactConfig,
-];
+]
